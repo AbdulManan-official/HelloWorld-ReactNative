bc973e0d72404f3719e94b50abfe4e3badea7177
Abdul Manan <abdullmanann7777@gmail.com>
2025-08-29 16:55:34 +0500

    Task 1: Hello World App completed

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import Toast from "react-native-toast-message";
 
 export default function App() {
+  const showtoast = () => {
+    Toast.show({
+      type: "success",
+      text1: "Nice to meet you! 👋",
+      position: "bottom",
+      visibilityTime: 2000,
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.screentext}>
+        Hello! Asslamualikum I am Abdul Manan
+      </Text>
+      <Text style={styles.screentext}>
+        I can “read” and “understand” over 100 languages 🤯
+      </Text>
+      <Text style={styles.screentext}>
+        but unlike humans, I never forget the syntax of any!
+      </Text>
+
+      <Image style={styles.profile} source={require("./assets/pic.jpeg")} />
+
+      <TouchableOpacity style={styles.button} onPress={showtoast}>
+        <Text style={styles.buttonText}>Press me</Text>
+      </TouchableOpacity>
+      <Toast />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +39,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "lightgrey",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  screentext: {
+    fontWeight: "bold",
+    paddingBottom: 10, 
+  },
+  profile: {
+    marginTop: 20, 
+    width: 300, 
+    height: 300, 
+    borderRadius: 150, 
+  },
+  button: {
+    marginTop: 25,
+    backgroundColor: "black",
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
